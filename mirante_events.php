758c0dd6c14ab7d6589f04411cce464de9d87edf
Lucas Dantas <lucasdantassenac@gmail.com>
2024-02-19 16:38:01 -0300

    first commit

---
+++ mirante_events.php
@@ -0,0 +1,14 @@
+<?php 
+/*
+ * Plugin Name:       Eventos do Mirante
+ * Plugin URI:        https://github.com/lucasdantassenac/wp_mirante_events
+ * Description:       Plugin que adiciona CPT de eventos do Mirante
+ * Version:           0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            R&D Marketing Digital
+ * Author URI:        https://rdmarketing.com.br/
+ */
+
+
+ require('ABSPATH' ) or die();
